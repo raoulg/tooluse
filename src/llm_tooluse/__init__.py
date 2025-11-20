27e431030e0e2cf16afb6ef475c2ce315eaf471b
raoul grouls <Raoul.Grouls@han.nl>
2025-11-20 16:06:26 +0100

    update init with imports

--- src/llm_tooluse/__init__.py
+++ src/llm_tooluse/__init__.py
@@ -1,7 +1,19 @@
 import sys
-
 from loguru import logger
+
+from llm_tooluse.mcp_adapter import MCPToolReference
+from llm_tooluse.mcp_client import MCPConnectionManager
+from llm_tooluse.tools import MCPToolLoader, ToolCollection, ToolRegistry
 
 logger.remove()
 logger.add(sys.stderr, level="WARNING")
 logger.add("logs/logfile.log", level="DEBUG")
+
+
+__all__ = [
+    "MCPToolReference",
+    "MCPConnectionManager",
+    "MCPToolLoader",
+    "ToolCollection",
+    "ToolRegistry",
+]
