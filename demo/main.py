6df3b22b9e34563e68ff97910ee26ddd3f247c38
raoul grouls <Raoul.Grouls@han.nl>
2025-11-20 13:31:38 +0100

    clean up demos

--- demo/main.py
+++ demo/main.py
@@ -3,22 +3,19 @@ from pathlib import Path
 from dotenv import load_dotenv
 from loguru import logger
 
-from llm_tooluse.calculator import add, subtract
+from calculator import add, subtract
 from llm_tooluse.llm import LLMClient
 from llm_tooluse.schemagenerators import BasicSchemaGenerator, LLMSchemaGenerator
 from llm_tooluse.settings import ModelConfig
 from llm_tooluse.tools import Tool, ToolCollection, ToolFactory
+import sys
+
+logger.remove()
+logger.add(sys.stdout, level="INFO")
 
 
 def main():
     load_dotenv()
-    basic_generator = BasicSchemaGenerator()
-    basic_schema = basic_generator.generate_schema(add)
-    logger.info(f"basic schema: {basic_schema.to_dict()}")
-
-    config = ModelConfig.from_toml(Path("config.toml"))
-    llm = LLMClient(config)
-    llm_generator = LLMSchemaGenerator(llm)
 
     math_collection = [add, subtract]
     toollist = [Tool.from_function(f) for f in math_collection]
@@ -27,8 +24,16 @@ def main():
     logger.info(f"collection: {toolcollection}")
     logger.info(f"registry in collection {toolcollection._registry.available_tools}")
 
+    basic_generator = BasicSchemaGenerator()
+    basic_schema = basic_generator.generate_schema(add)
+    logger.info(f"basic schema: {basic_schema.to_dict()}")
+
+    config = ModelConfig.from_toml(Path("config.toml"))
+    llm = LLMClient(config)
+
+
     factory = ToolFactory(
-        schema_generator=llm_generator,
+        schema_generator=basic_generator,
         llm_client=llm,
     )
     tools = factory.create_collection(math_collection)
@@ -38,12 +43,10 @@ def main():
     #
     config = ModelConfig.from_toml(Path("config.toml"))
     llm = LLMClient(config)
-    content = "add 2 to 8. Only respond with the result"
+    content = "add 123 to 456. Only respond with the result"
     messages = [{"role": "user", "content": content}]
     response = llm(messages)
     logger.info(f"response : {response}")
-    # model = "claude-3-haiku-20240307"
-    # content = "Give me a haiku about a pet chicken"
 
 
 if __name__ == "__main__":
