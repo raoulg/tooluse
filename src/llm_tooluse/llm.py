a952aa88986ca3be284bad9b2ba04188495f7037
raoul grouls <Raoul.Grouls@han.nl>
2025-11-21 18:36:29 +0100

    isolate get_tools

--- src/llm_tooluse/llm.py
+++ src/llm_tooluse/llm.py
@@ -1,43 +1,43 @@
 from typing import Any
 
 from anthropic import Anthropic
+from loguru import logger
+from ollama import Client as OllamaClient
+
 from llm_tooluse.schemagenerators import AnthropicAdapter, LlamaAdapter
 from llm_tooluse.settings import ClientType, ModelConfig
 from llm_tooluse.tools import ToolCollection, ToolRegistry
-from loguru import logger
-from ollama import Client as OllamaClient
 
 
 class LLMClient:
     def __init__(self, config: ModelConfig):
         self.config: ModelConfig = config
         self.client = self._initialize_client()
-        # Get all available tools
+        self.allowed_tools = config.allowed_tools
+
+    def __repr__(self) -> str:
+        return f"LLMClient(model={self.config.model_type.value})"
+
+    def get_tools(self) -> ToolCollection:
         registry = ToolRegistry()
         all_tools = ToolCollection(registry.available_tools)
 
         # Filter tools per LLMclient based on config
-        if config.allowed_tools is None:
-            self.tools: ToolCollection = all_tools
-            logger.debug(f"All tools allowed: {self.tools.tool_names}")
+        if self.allowed_tools is None:
+            tools: ToolCollection = all_tools
+            logger.debug(f"All tools allowed: {tools.tool_names}")
         else:
             # Validate requested tools exist
-            unknown = set(config.allowed_tools) - registry.available_tools
+            unknown = set(self.allowed_tools) - registry.available_tools
             if unknown:
                 raise ValueError(f"Unknown tools requested: {unknown}")
 
             # Get all tools except those not in allowed_tools
-            excluded = registry.available_tools - set(config.allowed_tools)
+            excluded = registry.available_tools - set(self.allowed_tools)
             logger.debug(f"Excluded tools: {excluded}")
-            self.tools: ToolCollection = all_tools - excluded
-            logger.debug(f"Allowed tools: {self.tools}")
-
-    def __repr__(self) -> str:
-        return f"LLMClient(model={self.config.model_type.value},\ntools={self.tools.tool_names})"
-
-    @property
-    def get_tools(self) -> ToolCollection:
-        return self.tools
+            tools: ToolCollection = all_tools - excluded
+            logger.debug(f"Allowed tools: {tools}")
+        return tools
 
     def _initialize_client(self):
         if self.config.client_type == ClientType.ANTHROPIC:
@@ -76,7 +76,8 @@ class LLMClient:
         if self.config.client_type == ClientType.ANTHROPIC:
             logger.debug("Using Anthropic")
             adapter = AnthropicAdapter
-            schemas = self.tools.get_schemas()
+            tools = self.get_tools()
+            schemas = tools.get_schemas()
             logger.success("Retrieved tool schemas")
             tools = [adapter.format_schema(schema) for schema in schemas]
             logger.debug(f"Using Anthropic adapter with tools: {tools}")
@@ -90,7 +91,8 @@ class LLMClient:
         elif self.config.client_type == ClientType.OLLAMA:
             logger.debug("Using Ollama")
             adapter = LlamaAdapter
-            schemas = self.tools.get_schemas()
+            tools = self.get_tools()
+            schemas = tools.get_schemas()
             logger.success("Retrieved tool schemas")
             tools = [adapter.format_schema(schema) for schema in schemas]
             logger.debug(f"Using Ollama adapter with tools: {tools}")
@@ -111,30 +113,38 @@ class LLMClient:
 
             # Use the adapter to extract tool calls
             tool_calls = adapter.extract_tool_calls(response)
+            toolcollection = self.get_tools()
 
             if tool_calls:
                 for tool in tool_calls:
                     toolcall = adapter.parse_tool_call(tool)
-                    schema = self.tools[toolcall["name"]].get_schema()
+                    schema = toolcollection[toolcall["name"]].get_schema()
                     logger.debug(f"From tool {toolcall['name']} got schema: {schema}")
 
-                    for p in schema.parameters:
-                        logger.debug(f"Checking if {p.name} is nullable {p.nullable} for arg {toolcall['args'][p.name]}")
-                        if (
-                            p.name in toolcall["args"]
-                            and p.nullable
-                            and toolcall["args"][p.name] in [None, "", "null", "None"]
-                        ):
-                            toolcall["args"][p.name] = None
+                    # for p in schema.parameters:
+                    #     logger.debug(
+                    #         f"Checking if {p.name} is nullable {p.nullable} for arg {toolcall['args'][p.name]}"
+                    #     )
+                    #     if (
+                    #         p.name in toolcall["args"]
+                    #         and p.nullable
+                    #         and toolcall["args"][p.name] in [None, "", "null", "None"]
+                    #     ):
+                    #         toolcall["args"][p.name] = None
+
                     logger.info(
                         f"LLM decides to use tool: {toolcall['name']} with {toolcall['args']}"
                     )
                     try:
-                        if toolcall["name"] not in self.tools:
-                            logger.warning(f"Tool {toolcall['name']} not registered")
+                        if toolcall["name"] not in toolcollection:
+                            logger.warning(
+                                f"Tool {toolcall['name']} not allowed collection"
+                            )
                             continue
 
-                        output = await self.tools(toolcall["name"], **toolcall["args"])
+                        output = await toolcollection(
+                            toolcall["name"], **toolcall["args"]
+                        )
 
                         # Format tool response using the adapter
                         tool_response = adapter.format_tool_response(toolcall, output)
